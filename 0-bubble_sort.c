b80a1b3c3ae8fdb38a36dbb3075528950749c00c
Paul Kirugu <pkirugu.chege111@gmail.com>
2023-11-21 17:00:01 +0000

    Bubble sort update 3 commit

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,8 +12,8 @@ void bubble_sort(int *array, size_t size)
 	int swap, temp;
 	size_t i, j;
 
-	/* Print the initial array */
-	print_array(array, size);
+	/* Print the initial array
+	print_array(array, size); */
 
 	for (i = 0; i < size - 1; i++)
 	{
